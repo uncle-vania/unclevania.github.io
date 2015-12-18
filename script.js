0b07b25ab656d918997af916966c227344e60576
n3tman <n3tman@users.noreply.github.com>
2015-12-18 09:58:11 +0300

    Update

---
+++ script.js
@@ -0,0 +1,23 @@
+VK.Widgets.Comments("vk_comments", {limit: 10, attach: "*"});
+
+var triggerScroll = false;
+
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 300) {
+        if (!triggerScroll) {
+            $('#scrollUp').fadeIn();
+            triggerScroll = true;
+        }
+    } else {
+        if (triggerScroll) {
+            $('#scrollUp').fadeOut();
+            triggerScroll = false;
+        }
+    }
+});
+
+$('#scrollUp').click(function(){
+    $('html, body').animate({
+        scrollTop: 0
+    });
+});
