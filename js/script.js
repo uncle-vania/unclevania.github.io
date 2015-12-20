c18fd5c259a53353f1002d3af4827ed3711e18f4
n3tman <n3tman@users.noreply.github.com>
2015-12-20 17:57:04 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-swfobject.embedSWF("img/christmastree.swf", "tree", "104", "152", "9.0.0", null, null, {wmode:"transparent"});
-swfobject.embedSWF("img/christmastree.swf", "tree2", "104", "152", "9.0.0", null, null, {wmode:"transparent"});
+/*swfobject.embedSWF("img/christmastree.swf", "tree", "104", "152", "9.0.0", null, null, {wmode:"transparent"});
+swfobject.embedSWF("img/christmastree.swf", "tree2", "104", "152", "9.0.0", null, null, {wmode:"transparent"});*/
 
 VK.Widgets.Comments("vk_comments", {limit: 20, attach: "*", autoPublish: 0});
 
