4861c96616605bbfb8ae96839144fba57b5193fc
n3tman <n3tman@users.noreply.github.com>
2015-12-20 11:34:22 +0300

    Update

--- js/snow.js
+++ js/snow.js
@@ -5,7 +5,7 @@ window.onload = function(){
 
     //canvas dimensions
     var W = window.innerWidth;
-    var H = 2000;
+    var H = window.innerHeight;
     canvas.width = W;
     canvas.height = H;
 
