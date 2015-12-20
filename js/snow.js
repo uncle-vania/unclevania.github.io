63c156e969a555c05cd60e9d93012946a99bc027
n3tman <n3tman@users.noreply.github.com>
2015-12-20 11:36:46 +0300

    Update

--- js/snow.js
+++ js/snow.js
@@ -10,7 +10,7 @@ window.onload = function(){
     canvas.height = H;
 
     //snowflake particles
-    var mp = 50; //max particles
+    var mp = 40; //max particles
     var particles = [];
     for(var i = 0; i < mp; i++)
     {
